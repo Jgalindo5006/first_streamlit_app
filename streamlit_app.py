b078d22e50292dd39e0039b570ca6e7b9551a7e7
Jgalindo5006 <jgalindo5006@gmail.com>
2023-02-05 17:26:27 +1030

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-def get_fruitvice_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     streamlit.text(fruityvice_response.json())
     fruityvice_normalized = pandas.json_normalize(fruityice_response.json())
